cd8ef53bcd05ad2404c38902a7fe0d6d13bc09fb
alibrahimzada <alibrahimzada@gmail.com>
2021-12-29 10:00:23 +0300

    added to_dec function in assembly

--- Assignment 2/Logisim/assembly.asm
+++ Assignment 2/Logisim/assembly.asm
@@ -1,6 +1,5 @@
 .data
 .code
-            ldi 0 0x0000
             ldi 6 0x0000
             ldi 7 0x0700
 main_loop   ldi 1 0x6000
@@ -38,6 +37,63 @@ mult_cond   dec 5
             jmp mult_loop
 mult_zero   ldi 6 0x0000
 mult_ret    ret
-to_dec      ldi 1 0x6004
-            st 1 6
+to_dec      ldi 4 0x0010
+            mov 1 6
+            ldi 2 0x0000
+alt_loop    ldi 0 0x000f
+            and 0 0 2
+            ldi 5 0x0004
+            sub 0 5 0
+            ldi 5 0x8000
+            and 0 0 5
+            jz check2
+            jmp add3_1
+check2      ldi 0 0x00f0
+            and 0 0 2
+            ldi 5 0x0040
+            sub 0 5 0
+            ldi 5 0x8000
+            and 0 0 5
+            jz check3
+            jmp add3_2
+check3      ldi 0 0x0f00
+            and 0 0 2
+            ldi 5 0x0400
+            sub 0 5 0
+            ldi 5 0x8000
+            and 0 0 5
+            jz check4
+            jmp add3_3
+check4      ldi 0 0xf000
+            and 0 0 2
+            ldi 5 0x4000
+            sub 0 5 0
+            ldi 5 0x8000
+            and 0 0 5
+            jz phase2
+            jmp add3_4
+phase2      add 2 2 2
+            ldi 5 0x8000
+            and 0 1 5
+            jz msb0
+            ldi 5 0x0001
+            add 2 2 5
+msb0        add 1 1 1
+            dec 4
+            jz display
+            jmp alt_loop
+display     ldi 1 0x6004
+            st 1 2
             ret
+add3_1      ldi 5 0x0003
+            add 2 2 5
+            jmp check2
+add3_2      ldi 5 0x0030
+            add 2 2 5
+            jmp check3
+add3_3      ldi 5 0x0300
+            add 2 2 5
+            jmp check4
+add3_4      ldi 5 0x3000
+            add 2 2 5
+            jmp phase2
